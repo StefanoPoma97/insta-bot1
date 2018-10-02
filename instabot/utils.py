ac37d8a41edc33984f44304158ceae3a2f9f82d0
Stefano <s.poma001@studenti.unibs.it>
2018-10-02 15:05:02 +0200

    print

--- instabot/utils.py
+++ instabot/utils.py
@@ -51,7 +51,7 @@ class file(object):
             self.save_list(items)
 
     def random(self):
-        print("cosa contiene la lista: " + self.list)
+        print("cosa contiene la lista: " + str(self.list))
         return random.choice(self.list)
 
     def remove_duplicates(self):
