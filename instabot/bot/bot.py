f2045be02c5ba5b4ddb47a5b046d4f78abb7fae1
Stefano <s.poma001@studenti.unibs.it>
2018-11-04 20:32:54 +0100

    commentato in check_user in bot

--- instabot/bot/bot.py
+++ instabot/bot/bot.py
@@ -597,14 +597,16 @@ class Bot(object):
     def check_media(self, media):
         return check_media(self, media)
 
-    def check_user(self, user, unfollowing=False):
-        return check_user(self, user, unfollowing)
+    def check_user(self, user, filter_closed_acc=False, unfollowing=False):
+        return check_user(self, user, filter_closed_acc, unfollowing)
+
+    #def check_user(self, user, unfollowing=False):
+     #   return check_user(self, user, unfollowing)
 
     def check_not_bot(self, user):
         return check_not_bot(self, user)
 
-    def check_user(self, user, filter_closed_acc=False, unfollowing=False):
-        return check_user(self, user, filter_closed_acc, unfollowing)
+
 
     # support
 
